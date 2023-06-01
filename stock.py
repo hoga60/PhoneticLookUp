b531f8cf151000d59f5c8a115bc13f067ff60dbd
hoga60 <71911755+hoga60@users.noreply.github.com>
2023-06-01 13:06:29 +0800

    Add files via upload

---
+++ stock.py
@@ -0,0 +1,26 @@
+import twstock
+import matplotlib.pyplot as plt
+import time
+from datetime import datetime
+
+def read( code ):
+    time.sleep(5)
+
+    stock = twstock.Stock( code )
+
+    x = stock.date
+    y = stock.price
+    z = stock.capacity
+
+    # 將日期轉成字串
+    x = [datetime.strftime(e, '%Y-%m-%d') for e in x]
+    xlabel = x[::10]
+
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+
+    ax1.plot( x, y, color="orange" )
+    ax2.bar( x, z )
+
+    plt.xticks( xlabel )
+    plt.savefig( code + '.png')
